fe90eddab9df75ac98ab569caf350195ca32eb5b
Kshitij <kshitij@localhost.localdomain>
2019-12-19 15:11:13 +0530

    Added a second print output line

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	System.out.println( "This is a Maven build Jenkins deployed project cloned from git repository!");
+
     }
 }
